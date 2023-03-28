2f5a44577596e3bcb2c2433de0068f0a09c69a38
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2023-03-28 14:46:51 +0200

    22.1 Relacja OneToMany w Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UpsertProductRequest;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -32,7 +33,9 @@ class ProductController extends Controller
      */
     public function create(): View
     {
-        return view("products.create");
+        return view("products.create", [
+            'categories' => ProductCategory::all()
+        ]);
     }
 
     /**
@@ -58,7 +61,8 @@ class ProductController extends Controller
     public function show(Product $product): View
     {
         return view("products.show", [
-            'product' => $product
+            'product' => $product,
+            'categories' => ProductCategory::all()
         ]);
     }
 
@@ -70,7 +74,8 @@ class ProductController extends Controller
     public function edit(Product $product): View
     {
         return view("products.edit", [
-            'product' => $product
+            'product' => $product,
+            'categories' => ProductCategory::all()
         ]);
     }
 
