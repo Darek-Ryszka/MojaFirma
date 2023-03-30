734d34159430b112acca228f6c650b2b3c2e906e
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2023-03-30 11:04:20 +0200

    26.1 Wysyłka maili po rejestracji użytkownika

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.9/dist/sweetalert2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
 
 @extends('layouts.app')
 
@@ -39,6 +41,7 @@
 
 @section('javascript')
     const deleteUrl = "{{ url('users') }}/";
+    const confirmDelete = "{{ __('shop.messages.delete_confirm') }}";
 @endsection
 
 @section('js-files')
