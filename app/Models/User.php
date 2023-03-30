918366eefcb81a38b53670c61f858bcdb8b4f6b4
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2023-03-30 23:18:31 +0200

    35.1 Relacja OneToOne w Laravel Eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,15 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,4 +43,14 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function address(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
+
+    public function hasAddress(): bool
+    {
+        return !is_null($this->address);
+    }
 }
